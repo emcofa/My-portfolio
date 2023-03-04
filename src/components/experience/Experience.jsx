ec94e5d13a8f66d167b044ff1ac21463882f4014
emcofah <emfh21@student.bth.se>
2023-03-04 20:04:04 +0100

    code for portfolio

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -97,7 +97,7 @@ const Experience = () => {
             <article className='experience__details'>
               <BsPatchCheckFill className='experience__details-icon' />
               <div>
-                <h4>MongoDB</h4>
+                <h4>Docker</h4>
                 <small className='text-light'>Intermediate</small>
               </div>
             </article>
